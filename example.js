3a9ef674ad065784f607b5b1105201dcd90fee84
Silvio Henrique Ferreira <25377830+shff@users.noreply.github.com>
2019-03-25 21:14:49 -0300

    Create example.js

---
+++ example.js
@@ -0,0 +1,19 @@
+const { spin } = require("./spin");
+
+(async function() {
+  const time = ms => new Promise(r => setTimeout(r, ms));
+
+  spin("Loading unicorns", 50);
+  await time(1000);
+  spin("Loading rainbows");
+  await time(1000);
+  spin("Loading with indent");
+  await time(1000);
+  spin("Loading with different spinners");
+  await time(1000);
+  spin();
+  await time(1000);
+  spin("Start again!");
+  await time(1000);
+  spin();
+})();
